99ee2b042f2745b139f151a171a99c8b3c314cfa
irinque <klenovmatvey@yandex.ru>
2025-02-13 23:09:28 +0300

    feat: plugin directory

--- src/main/java/me/irinque/CampfireChat/Main.java
+++ src/main/java/me/irinque/CampfireChat/Main.java
@@ -1,17 +1,28 @@
 package me.irinque.CampfireChat;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin
+{
+    public static Main instance;
+    public static Main getInstance() {return instance;}
+
+    private File directory_plugin = new File("plugins/CampfireChat");
+    public File get_directory_plugin() {return directory_plugin;}
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
+    public void onEnable()
+    {
+        if (instance == null) {instance = this;}
+        if (!get_directory_plugin().exists()) {get_directory_plugin().mkdirs();}
 
+        getServer().getLogger().info("§a[CampfireChat] Plugin is ready");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (instance != null) {instance = null;}
+        getServer().getLogger().info("§4[CampfireChat] Plugin was shutdown!");
     }
 }
